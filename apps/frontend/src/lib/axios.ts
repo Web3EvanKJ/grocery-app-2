a63fadb4e9ca9519480001f5dce4a6d96affe8a8
Evan Keane <web3evankeane@gmail.com>
2025-11-11 21:48:25 +0700

    Change env

--- apps/frontend/src/lib/axios.ts
+++ apps/frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
